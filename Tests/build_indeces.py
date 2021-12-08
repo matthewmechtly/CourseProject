6877671722d4338374c29371a3a9cf85b650ffe6
Sean Enright <seanrenright@gmail.com>
2021-12-07 23:25:31 -0800

    build datasets and config files dynamically

--- Tests/build_indeces.py
+++ Tests/build_indeces.py
@@ -1,46 +1,57 @@
+import fileinput
 from pathlib import Path
 import shutil
 
 from bs4 import BeautifulSoup
-import metapy
 
-# Run script out of 'Tests' folder
+# Run this script out of 'Tests' folder to create the inverted indeces for the webpages
+# in the "references_pages" folder. Metapy will not identify the correct directories if this is
+# run out of the project top-level directory.
 
-script_path = Path(__file__).parent
-ref_path = script_path / 'reference_pages'
-line_file_path = script_path / 'line.toml'
+# Global paths
+SCRIPT_PATH = Path(__file__).parent
+REFERENCE_PAGES_PATH = SCRIPT_PATH / 'reference_pages'
+LINE_FILE_PATH = SCRIPT_PATH / 'line.toml'
+CONFIG_FILE_SOURCE_PATH = SCRIPT_PATH / 'config.toml'
+CONFIG_DIR = SCRIPT_PATH / 'config_files'
+CONFIG_FILE_DEST_PATH = CONFIG_DIR / 'config.toml'
+
+CONFIG_REPLACE_STR = 'webpagename'
+
+
+def make_config_file(page_name):
+    # Copy /Tests/config.toml to /Tests/config_files/config.toml and insert name of
+    # dataset directory into config file
+    with CONFIG_FILE_DEST_PATH.open(mode='w', encoding='utf-8') as new_file:
+        with CONFIG_FILE_SOURCE_PATH.open(mode='r', encoding='utf-8') as old_file:
+            for line in old_file:
+                if CONFIG_REPLACE_STR in line:
+                    line = line.replace(CONFIG_REPLACE_STR, page_name)
+                new_file.write(line)
 
 def build_index_from_webpage(path):
     with path.open(mode='r', encoding='utf-8') as fp:        
         soup = BeautifulSoup(fp, 'html.parser')
         paragraphs = soup.find_all('p')
-        p = [p.getText().strip() for p in paragraphs if len(p) > 1]
+        paragraph_words = [p.getText().strip() for p in paragraphs if len(p) > 1]
         # Create directory to write website data
-        dataset_dir_path = script_path / path.stem
-        dataset_dir_path.mkdir(parents=False, exist_ok=True)
-        data_file_name = str(path.stem) + '.dat'
+        # Path will be /Tests/indeces/dataset/[HTML file name]
+        dataset_dir_path = SCRIPT_PATH / 'indeces' / path.stem / 'dataset'
+        dataset_dir_path.mkdir(parents=True, exist_ok=True)
+        data_file_name = 'dataset.dat'
         dataset_file_path = dataset_dir_path / data_file_name
         # copy line file to directory, since it is required by metapy
-        shutil.copy(str(line_file_path), str(dataset_dir_path / 'line.toml'))
+        shutil.copy(str(LINE_FILE_PATH), str(dataset_dir_path / 'line.toml'))
         # write website data to .dat file
         with dataset_file_path.open(mode='w', encoding='utf-8') as dp:
-            dp.write('\n'.join(p))
-        # generate inverted index folder and object
-        idx = metapy.index.make_inverted_index('config.toml')
-        submit_query(idx, 'industrial meat') # move to separate file
-
-def submit_query(index, query_terms):
-    ranker = metapy.index.OkapiBM25(2, 0.75)
-    query = metapy.index.Document()
-    query.content(query_terms)
-    top_docs = ranker.score(index, query, num_results=5)
-    print(top_docs)
-    for num, (d_id, _) in enumerate(top_docs):
-        content = index.metadata(d_id).get('content')
-        print("{}. {}...\n".format(num + 1, content))
+            dp.write('\n'.join(paragraph_words))
+        make_config_file(path.stem)
 
 def build_indeces():
-    ref_pages = ref_path.glob('*.htm')
+    # Create directory for customized config files at /Tests/config_files/
+    CONFIG_DIR.mkdir(parents=False, exist_ok=True) 
+    # Find all web page files in /Tests/reference_pages/
+    ref_pages = REFERENCE_PAGES_PATH.glob('*.htm')
     for p in ref_pages:
         build_index_from_webpage(p)
 
